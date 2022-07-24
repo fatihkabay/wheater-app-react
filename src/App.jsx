9b83be27f9d19b0e3216e5227b15e17d5bb6e9f5
Fatih Kabay <mfthkabay@gmail.com>
2022-07-24 11:05:15 +0300

    creating input and add-button

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='header'>
+         <div className='wheater-input'>
+          <input className='wheater-input-text' type="text" />
+         </div>
+         <div className='wheater-add'>
+          <button className='wheater-add-button'>Create</button>
+         </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
