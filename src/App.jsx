29a6f6004da368b94bfd7ef4726a92d47f151fc5
Fatih Kabay <mfthkabay@gmail.com>
2022-07-25 12:43:29 +0300

    edit css and new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './App.css';
-import './index.css';
-import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import "./index.css";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, {} from "react";
+import React, { useState } from "react";
 function App() {
   return (
-    <div className='container'>
-      
-      <div className='header'>
-         <div className='wheater-input'>
-          <input className='wheater-input-text'placeholder='Add one city...' type="text" />
-         </div>
-         <div className='wheater-add'>
-          <button className='wheater-add-button'><FontAwesomeIcon className="wheater-add-button-icon"
-           icon={faPlus} /></button>
-         </div>
-      </div><hr/>
+    <div className="container">
+      <div className="wheater-text">
+        <input
+          className="wheater-input"
+          placeholder="Add one city..."
+          type="text"
+        />
+        <button className="wheater-add">
+          <FontAwesomeIcon className="wheater-add-button-icon" icon={faPlus} />
+        </button>
+      </div>
+      <hr />
     </div>
   );
 }
