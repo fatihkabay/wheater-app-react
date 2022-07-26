6017e0d1397f46e4699ba3690f3d87c76de98361
Fatih Kabay <mfthkabay@gmail.com>
2022-07-26 12:04:45 +0300

    new css file

--- src/Wheather.jsx
+++ src/Wheather.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Wheather() {
   return (
 <div className='list-container'>
-    <div className='first-day'>gunesli</div>
-    <div className='second-day'>yagmurlu</div>
-    <div className='third-day'>sisli</div>
+    <div className='first-day'>Date</div>
+    <div className='second-day'>Wheater Status</div>
+    <div className='third-day'>Min Temp C / Max Temp C</div>
 </div> 
 )
 }
