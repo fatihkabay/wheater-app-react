40f790120b88afedc85a70d49cdd881e5d59b8b0
Fatih Kabay <mfthkabay@gmail.com>
2022-07-26 17:10:42 +0300

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
-import "./Wheather.jsx"
-import React from "react";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Wheather from "./Wheather.jsx";
 
 function App() {
+  const APP_KEY = "4976eeabaf7744af88980354222507";
+  let cityinput = "";
+  const [wheatherdata, setwheatherdata] = useState([]);
+  function citytext() {
+    document.querySelector("input").addEventListener("input", (e) => {
+      e.preventDefault();
+      cityinput = e.target.value;
+      console.log(cityinput);
+    });
+  }
+  async function getdata(value) {
+    const data = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no`
+    );
+    const result = await data.json();
+    setwheatherdata(result.forecast.forecastday);
+    console.log(result.forecast.forecastday);
+  }
   return (
     <div className="container">
       <div className="title-text">
@@ -15,15 +32,29 @@ function App() {
       <div className="wheater-text">
         <input
           className="wheater-input"
-          placeholder="Add one city..."
+          placeholder="Search a city..."
           type="text"
+          onChange={citytext}
         />
-        <button className="wheater-add">
-          <FontAwesomeIcon className="wheater-add-button-icon" icon={faPlus} />
+        <button className="wheater-add" onClick={() => getdata(cityinput)}>
+          <FontAwesomeIcon
+            className="wheater-add-button-icon"
+            icon={faMagnifyingGlass}
+          />
         </button>
       </div>
       <hr />
-     <Wheather/>
+      {wheatherdata.map((item) => (
+        <Wheather
+          key={item.date}
+          date={item.date}
+          mintemp={item.day.mintemp_c}
+          maxtemp={item.day.maxtemp_c}
+          condition={item.day.condition.text}
+          icon={item.day.condition.icon}
+
+        />
+      ))}
     </div>
   );
 }
--- src/Wheather.jsx
+++ src/Wheather.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Wheather.css";
 
-function Wheather() {
+function Wheather({date,mintemp,maxtemp,condition,icon}) {
   return (
-<div className='list-container'>
-    <div className='first-day'>Date</div>
-    <div className='second-day'>Wheater Status</div>
-    <div className='third-day'>Min Temp C / Max Temp C</div>
-</div> 
-)
+    <div className="list-container">
+      <div className="lists">
+        <div className="date">{date}</div>
+        <div><img src={icon} alt="is the tag that specifies the icons" /></div>
+        <div>{condition}</div>
+        <div>{mintemp} C / {maxtemp} C</div>
+      </div>
+    </div>
+  );
 }
 
-export default Wheather
+export default Wheather;
